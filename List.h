3577de0b5615720fd2441e3f6b1171d7b61c7804
Latygin <denisss2221@gmail.com>
2018-09-03 23:51:46 +0700

    Добавлены основные методы по лабе

--- List.h
+++ List.h
@@ -57,28 +57,79 @@ public:
             node=node->next;
             return *this;
         };
-      //  bool getState();
+        bool isEnd(){
+            return node->next == NULL;
+        };
+
+        bool isBegin(){
+            return node->prev == NULL;
+        }
+
         T& operator*() { return node->data; }
         friend class List<T>;
 
     };
 
-    int getListSize(){
-        return 0;
+    int getSize(){
+        return size;
     }
 
     void cleanList(){
-
+        Node *node = head->next;
+        while (node){
+            Node *buf = node;
+            buf = buf->next;
+            delete node;
+            node = buf;
+        }
+        head->next = NULL;
+        head->prev = NULL;
+        size = 0;
     }
 
     bool isEmpty(){
-        return false;
+        return size <= 0;
     }
 
     bool contain(T t){
+        Node *node = head->next;
+        while (node){
+            if (node->data == t) {
+                return true;
+            }
+            node = node->next;
+        }
         return false;
     }
-    void append(T data){ //Добавить в конец
+
+    T getValueByNumber(int number){
+
+        return getNodeByNumber(number)->data;
+    }
+
+    void updateValueByNumber(int number, T newData){
+        getNodeByNumber(number)->data = newData;
+    }
+
+    Node * getNodeByNumber(int number){
+        Node *node;
+        if (number/size > 0.5){
+            node = head->prev;
+            for (int i=0; i<size-number; i++){
+                if (i == number){ break;}
+                node = node->prev;
+            }
+        } else{
+            node = head->next;
+            for (int i=0; i<number; i++){
+                if (i == number){ break;}
+                node = node->next;
+            }
+        }
+        return node;
+    }
+
+    void addToEnd(T data){ //Добавить в конец
         Node *node = new Node();
         node->data = data;
 
@@ -87,9 +138,77 @@ public:
             head->next= node;
         } else{
             head->prev->next = node;
+            node->prev = head->prev;
             head->prev = node;
         }
         size++;
+    }
+
+    void addToStart(T data){ //Добавить в начало
+        Node *node = new Node();
+        node->data = data;
+
+        if (head->prev == NULL) {
+            head->prev= node;
+            head->next= node;
+        } else{
+            head->next->prev = node;
+            node->next = head->next;
+            head->next = node;
+        }
+        size++;
+    }
+
+    void addNodeByNumber(int number, T data){
+        if (number == size ){
+            addToEnd(data);
+        } else if (number == 0){
+            addToStart(data);
+        } else{
+            Node *node = new Node;
+            node->data = data;
+            Node *buf= head->next;
+            for (int i=0; i<number; i++){
+                buf = buf->next;
+                cout<<buf->data<<endl;
+            }
+
+            node->next = buf;
+            buf->prev->next = node;
+            node->prev = buf->prev;
+            buf->prev = node;
+        }
+    }
+
+    void deleteNodeByValue(T data){
+            Node *node = head->next;
+            while (node){
+                if (node->data == data) {
+                    deleteNode(node);
+                    break;
+                }
+                node = node->next;
+            }
+    }
+
+    void deleteNode(Node *node){
+        if (node->prev == NULL){
+            head->next = node->next;
+            node->next->prev = NULL;
+            delete node;
+        } else if (node->next == NULL){
+            head->prev = node->prev;
+            node->prev->next = NULL;
+            delete node;
+        } else{
+            node->next->prev = node->prev;
+            node->prev->next = node->next;
+            delete node;
+        }
+    }
+
+    void deleteNodeByNumber(int number){
+        deleteNode(getNodeByNumber(number));
     }
 
     void show(){
@@ -98,12 +217,26 @@ public:
             cout<<node->data<<" ";
             node = node->next;
         }
+        cout<<endl;
+    }
+    int getPosByValue(T value){
+        Node *node = head->next;
+        int position=0;
+        while (node){
+            if (node->data == value){
+                return position;
+            }
+            node = node->next;
+            position++;
+        }
+        return -1;
     }
 
 
 
     virtual ~List() {
-
+        cleanList();
+        delete head;
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -6,13 +6,47 @@ using namespace std;
 int main() {
     List<int> myList;
     List<int>::Iterator i(&myList);
-    myList.append(1);
-    myList.append(2);
-    myList.append(3);
+
+    myList.addToEnd(10);
+    myList.addToEnd(2);
+    myList.addToEnd(3);
     myList.show();
     i.first();
-    cout<<*i<<endl;
+    cout << *i << endl;
     i.last();
-    cout<<*i<<endl;
+    cout << *i << endl;
+    *i = 100;
+    myList.show();
+
+    if (myList.contain(100)) {
+        cout << myList.getValueByNumber(0) << endl;
+    }
+    myList.show();
+    myList.cleanList();
+    myList.show();
+
+    myList.addToEnd(1);
+    myList.addToEnd(2);
+    myList.addToEnd(3);
+    myList.show();
+    myList.updateValueByNumber(2, 76);
+    cout << myList.getValueByNumber(2) << endl;
+    myList.show();
+    cout << myList.getPosByValue(78) << endl;
+    myList.addToStart(677);
+    myList.show();
+    myList.addNodeByNumber(4, 100);
+    myList.show();
+    myList.deleteNodeByValue(2);
+    myList.show();
+    myList.deleteNodeByValue(100);
+    myList.show();
+    myList.deleteNodeByNumber(1);
+    myList.show();
+    myList.deleteNodeByNumber(0);
+    myList.show();
+    myList.deleteNodeByNumber(0);
+    myList.show();
+
     return 0;
 }
